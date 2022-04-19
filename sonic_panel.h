8ca6015389b8e2c906b6015c9062b128a11118e7
tom <shaolang@163.com>
2022-04-19 22:57:53 +0800

    rename the sonic_visualizer.h -> sonic_panel.h

---
+++ sonic_panel.h
@@ -0,0 +1,44 @@
+#ifndef SONIC_VISUALIZER_H
+#define SONIC_VISUALIZER_H
+
+#include <QWidget>
+#include <QColor>
+#include <QHash>
+#include <QPainterPath>
+#include <QGraphicsScene>
+#include <QFrame>
+#include <QResizeEvent>
+#include <QSvgWidget>
+#include <QGraphicsView>
+
+
+class sonic_panel:public QFrame
+
+{
+    Q_OBJECT
+public:
+    sonic_panel();
+
+    void paint(QPainter &p);
+    void change_d();
+protected:
+    void paintEvent(QPaintEvent *event) override;
+    void resizeEvent(QResizeEvent *event) override;
+
+private:
+    int w;   // this->width()
+    int h;   // this->heigth()
+    int d;
+
+    QGraphicsScene *sonic_scene = nullptr;
+    QGraphicsView *sonic_view = nullptr;
+
+    QPainterPath sonic_wave_path;
+    QPainterPath horizontal_tick;
+
+};
+
+
+
+
+#endif // SONIC_VISUALIZER_H
